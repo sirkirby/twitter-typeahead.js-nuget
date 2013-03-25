4b01882ac56a46888ab69367e81f6f6966d1a477
Chris Kirby <chrisk@ryantechinc.com>
2013-03-25 09:31:53 -0700

    updated to v0.9.0 release
    removed the css files from the main package since they are no longer
    needed. updated both packages with change info and removed the style
    bundle code from the mvc package. both have been published to nuget.org.

--- Twitter.Typeahead.MVC/TypeaheadBundleConfig.cs.pp
+++ Twitter.Typeahead.MVC/TypeaheadBundleConfig.cs.pp
@@ -8,11 +8,9 @@ namespace $rootnamespace$.App_Start
 	{
 		public static void RegisterBundles()
 		{
-			// Add @Styles.Render("~/Content/typeahead") in the <head/> of your _Layout.cshtml view
 			// Add @Scripts.Render("~/bundles/typeahead") after jQuery in your _Layout.cshtml view
 			// When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
 			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/typeahead").Include("~/Scripts/typeahead*"));
-			BundleTable.Bundles.Add(new StyleBundle("~/Content/typeahead").Include("~/Content/typeahead*"));
 		}
 	}
 }
