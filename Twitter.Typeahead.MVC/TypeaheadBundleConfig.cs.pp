eaf5d8e6828d93a4a0e49fff3854275a4f8953eb
Chris Kirby <chris@infinitusdev.com>
2014-02-07 10:37:00 -0800

    updated all source to latest release. updated both nuspec's with new content, logos, links, descriptions, and dependencies. updated readme's.

--- Twitter.Typeahead.MVC/TypeaheadBundleConfig.cs.pp
+++ Twitter.Typeahead.MVC/TypeaheadBundleConfig.cs.pp
@@ -9,8 +9,10 @@ namespace $rootnamespace$.App_Start
 		public static void RegisterBundles()
 		{
 			// Add @Scripts.Render("~/bundles/typeahead") after jQuery in your _Layout.cshtml view
-			// When <compilation debug="true" />, MVC4 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
-			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/typeahead").Include("~/Scripts/typeahead*"));
+			// When <compilation debug="true" />, MVC 5 will render the full readable version. When set to <compilation debug="false" />, the minified version will be rendered automatically
+			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/typeahead").Include("~/Scripts/typeahead.bundle*"));
+			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/typeahead-bloodhound").Include("~/Scripts/bloodhound*"));
+			BundleTable.Bundles.Add(new ScriptBundle("~/bundles/typeahead-jquery").Include("~/Scripts/typeahead.jquery*"));
 		}
 	}
 }
